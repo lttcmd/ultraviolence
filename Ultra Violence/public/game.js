5581bb072a502127b3fcf1350f818767db5f40f7
Dylan <dylanjayparker@gmail.com>
2025-07-14 18:17:57 +1000

    Add files via upload

---
+++ Ultra Violence/public/game.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const socket = new WebSocket(`ws://${location.host}`);
+let playerId = null;
+let players = [];
+
+const keys = {};
+
+document.addEventListener('keydown', (e) => keys[e.key] = true);
+document.addEventListener('keyup', (e) => keys[e.key] = false);
+
+socket.addEventListener('message', (e) => {
+  const data = JSON.parse(e.data);
+  if (data.type === 'init') {
+    playerId = data.id;
+  } else if (data.type === 'state') {
+    players = data.players;
+  }
+});
+
+function sendMovement() {
+  let dx = 0, dy = 0;
+  if (keys['w']) dy = -2;
+  if (keys['s']) dy = 2;
+  if (keys['a']) dx = -2;
+  if (keys['d']) dx = 2;
+  if (dx !== 0 || dy !== 0) {
+    socket.send(JSON.stringify({ type: 'move', dx, dy }));
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const p of players) {
+    ctx.fillStyle = p.id === playerId ? 'lime' : 'red';
+    ctx.fillRect(p.x, p.y, 20, 20);
+  }
+}
+
+function gameLoop() {
+  sendMovement();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
