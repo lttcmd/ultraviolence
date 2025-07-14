2185df00372c17e89a2ae441cf69070aa56ce9de
Dylan <dylanjayparker@gmail.com>
2025-07-14 18:32:01 +1000

    Update server.js

--- server.js
+++ server.js
@@ -50,4 +50,4 @@ setInterval(() => {
   });
 }, 1000 / 30); // 30 FPS
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+server.listen(8080, () => console.log('Server running on port 8080'));
