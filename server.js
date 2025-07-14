e186de1c54fb8990819314fbc01716f809227a49
Dylan <dylanjayparker@gmail.com>
2025-07-14 19:08:04 +1000

    Update server.js

--- server.js
+++ server.js
@@ -84,4 +84,7 @@ setInterval(() => {
   });
 }, 1000 / 30); // 30 FPS
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 8080; // <-- This line is key
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}););
